548183c621147ad950fb6618222f2df6547e2786
Milan G <dev@milangress.de>
2023-11-25 11:14:15 +0100

    add block structure

---
+++ src/components/blockTxt.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let content = "Hello World";
+    export let isCurrent = false;
+    $: isCurrentClass = isCurrent ? 'current' : '';
+</script>
+<span class="{isCurrentClass}">{content}</span>
+
+
+<style>
+    .current {
+        color: blue;
+    }
+</style>
