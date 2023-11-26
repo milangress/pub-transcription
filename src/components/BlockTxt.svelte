54c6817e90e4a0b4e67482f31dca03b99ef5b2e8
Milan G <dev@milangress.de>
2023-11-27 00:32:29 +0100

    prepare midi controls

--- src/components/BlockTxt.svelte
+++ src/components/BlockTxt.svelte
@@ -1,13 +1,29 @@
 <script>
     export let content = "Hello World"
     export let isCurrent = false
-    export let settings = 1
+    export let settings = {}
+
+    function replaceVariables(str, settings) {
+        console.log(str, settings)
+        if (settings && Array.isArray(settings) && settings.length>0) {
+            settings.forEach(function (setting) {
+                str = str.replace(new RegExp(setting.var, 'g'), setting.value)
+            })
+            /*settings.forEach(setting => {
+                window[setting.var] = setting.value
+            })
+            str = eval('`'+str+'`')*/
+            console.log('replaced: ',str)
+        }
+        return str
+    }
     $: isCurrentClass = isCurrent ? 'current' : ''
+    $: replacedInlineStyles = replaceVariables(settings.inlineStyle, settings.controllerSettings)
 </script>
 
 <span
         class="{isCurrentClass}"
-        style="{settings.inlineStyle}"
+        style="{replacedInlineStyles}"
         style:font-size="{settings.fontSize}em"
 >
     {content}
