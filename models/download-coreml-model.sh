d20b8706185a453dd3ad72a89a875f55b8b6310d
Milan G <dev@milangress.de>
2023-11-29 03:11:36 +0100

    update ggml scripts

--- models/download-coreml-model.sh
+++ models/download-coreml-model.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# This script downloads Whisper model files that have already been converted to ggml format.
+# This script downloads Whisper model files that have already been converted to Core ML format.
 # This way you don't have to convert them yourself.
 
-#src="https://ggml.ggerganov.com"
-#pfx="ggml-model-whisper"
-
-src="https://huggingface.co/ggerganov/whisper.cpp"
+src="https://huggingface.co/datasets/ggerganov/whisper.cpp-coreml"
 pfx="resolve/main/ggml"
 
 # get the path of this script
 function get_script_path() {
     if [ -x "$(command -v realpath)" ]; then
-        echo "$(dirname "$(realpath "$0")")"
+        echo "$(dirname $(realpath $0))"
     else
         local ret="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)"
         echo "$ret"
@@ -22,29 +19,7 @@ function get_script_path() {
 models_path="$(get_script_path)"
 
 # Whisper models
-models=(
-    "tiny.en"
-    "tiny"
-    "tiny-q5_1"
-    "tiny.en-q5_1"
-    "base.en"
-    "base"
-    "base-q5_1"
-    "base.en-q5_1"
-    "small.en"
-    "small.en-tdrz"
-    "small"
-    "small-q5_1"
-    "small.en-q5_1"
-    "medium"
-    "medium.en"
-    "medium-q5_0"
-    "medium.en-q5_0"
-    "large-v1"
-    "large-v2"
-    "large-v3"
-    "large-q5_0"
-)
+models=( "tiny.en" "tiny" "base.en" "base" "small.en" "small" "medium.en" "medium" "large-v1" "large-v2" "large-v3" )
 
 # list available models
 function list_models {
@@ -72,27 +47,21 @@ if [[ ! " ${models[@]} " =~ " ${model} " ]]; then
     exit 1
 fi
 
-# check if model contains `tdrz` and update the src and pfx accordingly
-if [[ $model == *"tdrz"* ]]; then
-    src="https://huggingface.co/akashmjn/tinydiarize-whisper.cpp"
-    pfx="resolve/main/ggml"
-fi
+# download Core ML model
 
-# download ggml model
+printf "Downloading Core ML model $model from '$src' ...\n"
 
-printf "Downloading ggml model $model from '$src' ...\n"
+cd $models_path
 
-cd "$models_path"
-
-if [ -f "ggml-$model.bin" ]; then
+if [ -f "ggml-$model.mlmodel" ]; then
     printf "Model $model already exists. Skipping download.\n"
     exit 0
 fi
 
 if [ -x "$(command -v wget)" ]; then
-    wget --no-config --quiet --show-progress -O ggml-$model.bin $src/$pfx-$model.bin
+    wget --quiet --show-progress -O ggml-$model.mlmodel $src/$pfx-$model.mlmodel
 elif [ -x "$(command -v curl)" ]; then
-    curl -L --output ggml-$model.bin $src/$pfx-$model.bin
+    curl -L --output ggml-$model.mlmodel $src/$pfx-$model.mlmodel
 else
     printf "Either wget or curl is required to download models.\n"
     exit 1
@@ -100,12 +69,14 @@ fi
 
 
 if [ $? -ne 0 ]; then
-    printf "Failed to download ggml model $model \n"
+    printf "Failed to download Core ML model $model \n"
     printf "Please try again later or download the original Whisper model files and convert them yourself.\n"
     exit 1
 fi
 
-printf "Done! Model '$model' saved in 'models/ggml-$model.bin'\n"
+printf "Done! Model '$model' saved in 'models/ggml-$model.mlmodel'\n"
+printf "Run the following command to compile it:\n\n"
+printf "  $ xcrun coremlc compile ./models/ggml-$model.mlmodel ./models\n\n"
 printf "You can now use it like this:\n\n"
 printf "  $ ./main -m models/ggml-$model.bin -f samples/jfk.wav\n"
 printf "\n"
