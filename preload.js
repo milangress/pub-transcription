5cbc51f97f36b429232faa15f23f1b33c33039b6
Milan G <dev@milangress.de>
2024-02-01 08:50:19 +0100

    save and restore

--- preload.js
+++ preload.js
@@ -5,5 +5,9 @@ contextBridge.exposeInMainWorld('electronAPI', {
     onTransData: (callback) => ipcRenderer.on('trans-data', callback),
     onTransInfo: (callback) => ipcRenderer.on('trans-info', callback),
     printSuccess: (callback) => ipcRenderer.on('print-success', callback),
+    getStoreValue: (key) => ipcRenderer.invoke('getStoreValue', key),
+    setStoreValue: (key, value) => ipcRenderer.invoke('setStoreValue', key, value),
     print: (printSettings) => ipcRenderer.send('print', printSettings)
 })
+
+
