f462b1c1d45f2dea0aa8263ed5254b9e8fdcbbd3
Milan G <dev@milangress.de>
2025-01-30 17:24:24 +0100

    ✨ feat: Refactor inline styles with SASS-like variables and improved text styling

--- src/App.svelte
+++ src/App.svelte
@@ -52,27 +52,29 @@
 		controllerSettings: [...inputJson.controllers],
 		fontFamily: fontFamilys[0],
 		inlineStyle: `.el {
-   display: inline-block;
+  display: inline-block;
   //transform: skew(30deg, 2deg);
-  line-height: 1.8;
-  //filter: blur(calc(5px * m1));
-  //filter: drop-shadow(16px 16px 10px black);
-  //letter-spacing: 7px;
-  //text-decoration: green wavy underline;
-  //text-shadow: 2px 2px 10px red;
-  //text-shadow: 5px 5px #000;
-  //text-shadow: 1px 1px 2px red, 0 0 1em blue, 0 0 0.2em blue;
+  line-height: $m2 * 3;
+  // filter: drop-shadow(16px 16px 10px black);
+  letter-spacing: $m5 * 1px;
+  text-decoration: blue wavy underline $m1 * 5px;
+  text-shadow: 2px 2px 10px red;
+  // text-shadow: 5px 5px #000;
+  // text-shadow: 1px 1px 2px red, 0 0 1em blue, 0 0 0.2em blue;
 
-  //color: white;
+  
+  // color: white;
   text-shadow: 1px 1px 4px black, 0 0 1em black, 0 0 10px black;
 
-  filter: url(#outline);
+  // filter: url(#outline);
+  font-family: NIKITA-Regular;
 
-  background: rgba(bgR,bgG,bgB,bgA);
-  //color: rgba(bgR,bgG,bgB,bgA);
-  rotate: r1deg;
-  font-size:fszem;
+  background: rgba($bgR, $bgG, $bgB, $bgA);
+  rotate: $r1 * 1deg;
+  font-size: $fsz * 1em;
+  font-family: NIKITA-Regular;
 }
+
 `
 	}
 
