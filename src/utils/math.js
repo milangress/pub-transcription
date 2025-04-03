239690a5fa2ed80760825a57b3671e0299af8811
Milan G <dev@milangress.de>
2025-04-03 08:33:16 +0200

    refactor: enhance CodeEditor indentation logic, validate controller value type, and migrate math utilities to TypeScript

--- src/utils/math.js
+++
@@ -1,7 +0,0 @@
-export const mapRange = (value, x1, y1, x2, y2) => (value - x1) * (y2 - x2) / (y1 - x1) + x2;
-
-export const clamp = (value, min, max) => Math.max(min, Math.min(value, max));
-
-export const round = (value, precision = 2) => Math.round(value * Math.pow(10, precision)) / Math.pow(10, precision);
-
-export const getRandom = (min, max) => Math.random() * (max - min) + min;
