575965f16430f31b040c0f87826ab6db4b138d26
Milan G <dev@milangress.de>
2025-04-03 14:42:10 +0200

    refactor: replace component instantiation with mount function for improved clarity

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,8 +1,9 @@
+import { mount } from 'svelte'
 import App from './App.svelte'
 
 import './assets/global.css'
 
-const app = new App({
+const app = mount(App, {
   target: document.body
 })
 
--- src/renderer/src/window/print/main.ts
+++ src/renderer/src/window/print/main.ts
@@ -1,7 +1,6 @@
-import PrintWindow from './App.svelte'
+import { mount } from 'svelte'
+import App from './App.svelte'
 
-const app = new PrintWindow({
-  target: document.body
-})
+const app = mount(App, { target: document.body })
 
 export default app
