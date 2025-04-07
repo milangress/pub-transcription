7c6da11f8e7515ee2934e9030dcfe07c194954ed
Milan G <dev@milangress.de>
2025-04-07 19:01:20 +0200

    add usedebounce

---
+++ src/renderer/src/components/hooks/useDebounce.svelte.ts
@@ -0,0 +1,32 @@
+export const useDebounce = (
+  initialValue: unknown,
+  delay: number = 250
+): {
+  value: unknown
+  update: (newValue: unknown) => void
+  loading: boolean
+} => {
+  let timeout = $state<ReturnType<typeof setTimeout> | null>(null)
+  let value = $state<unknown>(initialValue)
+  let loading = $state<boolean>(false)
+
+  const update = (newValue: unknown): void => {
+    if (timeout) clearTimeout(timeout)
+    loading = true
+
+    timeout = setTimeout(() => {
+      value = newValue
+      loading = false
+    }, delay)
+  }
+
+  return {
+    get value(): unknown {
+      return value
+    },
+    update,
+    get loading(): boolean {
+      return loading
+    }
+  }
+}
